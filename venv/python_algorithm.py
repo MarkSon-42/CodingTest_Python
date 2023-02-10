7e6a56313dee5cc6588f90769397c9749cce682b
Son <mark42son@gmail.com>
2023-02-10 15:23:34 +0900

    add error solved

--- venv/python_algorithm.py
+++ venv/python_algorithm.py
@@ -32,3 +32,20 @@ for n in range(1, 10 + 1):
 print(a)
 
 # range
+
+print(list(range(5)))
+
+#       ->  [0, 1, 2, 3, 4]
+
+print(range(5))
+
+# range(0, 5)
+
+print(type(range(5)))
+
+# class 'range'
+
+for i in range(5):
+    print(i, end=' ')
+
+# enumerate
