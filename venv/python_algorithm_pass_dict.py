4c4f553e5aecc3a816be56fc040d62466d4f17e1
Son <mark42son@gmail.com>
2023-02-11 17:33:43 +0900

    update python pass ~ dict

--- venv/python_algorithm_pass_dict.py
+++ venv/python_algorithm_pass_dict.py
@@ -2,3 +2,17 @@
 
 # 나눗셈 연산자
 
+print(5 / 3)
+
+t = type(5 / 3)
+print(t)
+
+t_int = type(5 // 3)
+print(t_int)
+
+# 나눗셈 연산자 // 는 int(a / b)와 동일하다
+
+# 몫과 나머지를 동시에 구할 때 : divmod(5, 3)
+
+print(divmod(21, 4))
+
