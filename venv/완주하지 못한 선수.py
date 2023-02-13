16dd87803f8cfd6f12169c71d26c18687801de7b
Son <mark42son@gmail.com>
2023-02-13 14:33:22 +0900

    solve new problem at programmers

---
+++ venv/완주하지 못한 선수.py
@@ -0,0 +1,16 @@
+# 크로아티아 정보올림피아드 문제
+
+# 데이터는 그리 크지 않다.
+# 참가자 중에 동명이인이 있을 수 있다는 조건을 처리해야 한다. 분명 테스트케이스에 해당 케이스가 포함되어있을 것
+# 따라서 participant에 completion이 있다면 1:1로 대응해서 삭제해주는 작업이 필요하다.
+# 종이에 그려보니 규칙성 하나를 발견
+def solution(participant, completion):
+    answer = ''
+    participant.sort()
+    completion.sort()
+
+    for i in range(len(participant) - 1):
+        if participant[i] != completion[i]:
+            return participant[i]
+
+    return participant[len(participant) - 1]
