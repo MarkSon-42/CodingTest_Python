b9e4f9589ba949b00d362ffe9e660c1fdd83a48b
MarkSon-42 <mark42son@gmail.com>
2023-03-21 15:49:44 +0900

    list comprehension

---
+++ venv/grammar_python/IntigerSeparator.py
@@ -0,0 +1,18 @@
+a = 3250
+b = 18_797_929_839
+
+print(a)
+print(b)
+
+print(type(a))
+print(type(b))
+print(type(a) == type(b))
+
+# bonus!
+
+def some_function():
+    ...
+
+def another_function():
+    pass
+
--- venv/grammar_python/ListComprehension.py
+++ venv/grammar_python/ListComprehension.py
@@ -1,8 +1,61 @@
 # https://www.geeksforgeeks.org/python-list-comprehension/
 
+# newList = [expression(element) for element in oldList if condition]
+
 # Python List comprehension provides a much more short syntax for creating a new list based on the values of an existing list.
 
 List = [character for character in [1, 2, 3]]
 
 print(List)
+
+list = [i for i in range(11) if i % 2 == 0]
+print(list)
+
+matrix = [[j for j in range(5)] for j in range(5)]
+print(matrix)
+
+List = []
+
+for character in 'Geeks 4 Geeks!':
+    List.append(character)
+
+print(List)
+
+
+
+
+# Time Analysis in List Comprehensions and Loop
+import time
+
+def for_loop(n):
+    result = []
+    for i in range(n):
+        result.append(i**2)
+    return result
+
+
+
+def list_comprehension(n):
+    return [i**2 for i in range(n)]
+
+
+# Driver Code
+
+# Calculate time taken by for_loop()
+begin = time.time()
+for_loop(10 ** 6)
+end = time.time()
+
+# Display time taken by for_loop()
+print('Time taken for_loop:', round(end - begin, 2))
+
+# Calculate time takens by list_comprehension()
+begin = time.time()
+list_comprehension(10 ** 6)
+end = time.time()
+
+# Display time taken by for_loop()
+print('Time taken for list_comprehension:', round(end - begin, 2))
+
+
 
