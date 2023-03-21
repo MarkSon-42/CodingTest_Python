d2b19506ce6a411d5b6110089539b6f591eba132
MarkSon-42 <mark42son@gmail.com>
2023-03-21 15:25:13 +0900

    none

---
+++ venv/Programmers_Python/캐릭터의 좌표(ing..).py
@@ -0,0 +1,7 @@
+# [좌우, 상하] [0, 0]은 정 중앙에 위치
+
+def solution(keyinput, board):
+    limit_height = board[1]//2
+    limit_width = board[0]//2
+    answer = []
+    return answer
---
+++ venv/grammar_python/ListComprehension.py
@@ -0,0 +1,8 @@
+# https://www.geeksforgeeks.org/python-list-comprehension/
+
+# Python List comprehension provides a much more short syntax for creating a new list based on the values of an existing list.
+
+List = [character for character in [1, 2, 3]]
+
+print(List)
+
