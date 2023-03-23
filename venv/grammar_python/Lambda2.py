0cf61b6b94b4fc5ee9605c422f0069c2180b89e3
MarkSon-42 <mark42son@gmail.com>
2023-03-23 14:33:08 +0900

    additional lambda commit

--- venv/grammar_python/Lambda2.py
+++ venv/grammar_python/Lambda2.py
@@ -9,3 +9,10 @@ print("Sorted numerically:",
 print(list(filter(lambda x: not (int(x) % 2 == 0 and int(x) > 0), ln)))
 
 print(list(map(lambda x: str(int(x) + 10), ln)))
+
+
+my_list = [1, 2, 3, 4, 5]
+
+new_list = list(filter(lambda x: x % 2 != 0, my_list))
+
+print(new_list)
