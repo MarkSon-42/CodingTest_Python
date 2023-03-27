9eb7193cef53072105a21e65a5d3d5c9368b143c
MarkSon-42 <mark42son@gmail.com>
2023-03-27 15:54:58 +0900

    이진수 더하기

---
+++ venv/Programmers_Python/이진수 더하기.py
@@ -0,0 +1,10 @@
+# module 쓰지않고 그냥 구현으로 풀어보기 (... )
+# https://star7sss.tistory.com/326
+def solution(bin1, bin2):
+    answer = ''
+    # 맨 마지막꺼 부터 더해줘야 한다.
+    # hint : 2' -> 10'
+    # return 10' -> 2'
+    for i in bin:
+        pass
+    return 1
