7b20a05a1fe4c323d8cca0ef2db6028f4f1ff405
MarkSon-42 <mark42son@gmail.com>
2023-03-29 12:25:24 +0900

    record 75

--- venv/kookmin_Algolab/_03_계단오르기.py
+++ venv/kookmin_Algolab/_03_계단오르기.py
@@ -1,9 +1,15 @@
 t = int(input())
-
+answer = 0
 for i in range(t):
     n, p = map(int,input().split())
 
+    if p % 2 == 0:
+        answer = (n // 2) + 1
+        print(answer)
+    else:
+        answer = n // 2
+        print(answer)
 
-# 2칸 혹은 1칸
-# 홀짝으로 봐야함;
-#
+
+
+# 아래 코드는 75점 (why??)
