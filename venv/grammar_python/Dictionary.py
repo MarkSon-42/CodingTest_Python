755503f5f0769679d8c48806da578b7243af9cd2
MarkSon-42 <mark42son@gmail.com>
2023-03-29 13:05:19 +0900

    py basic

--- venv/grammar_python/Dictionary.py
+++ venv/grammar_python/Dictionary.py
@@ -4,3 +4,12 @@ print(Dict)
 Dict = {'Name': 'Geeks', 1: [1, 2, 3, 4]}
 print(Dict)
 
+# Nested dictionary : 딕셔너리 중첩도 가능
+
+Dict = {1: 'Geeks', 2: 'For', 3: {'A': 'Welcome', 'B': 'To', 'C': 'Geeks'}}
+print(Dict)
+
+# get() : time complexity : O(1)
+
+print(Dict.get(3))
+print(Dict.get(2))
