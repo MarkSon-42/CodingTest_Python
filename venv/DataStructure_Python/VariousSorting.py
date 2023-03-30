e7d84b12f83ae6418dd8f34a5da167364ace9f4b
MarkSon-42 <mark42son@gmail.com>
2023-03-30 14:43:48 +0900

    sort with python

--- venv/DataStructure_Python/VariousSorting.py
+++ venv/DataStructure_Python/VariousSorting.py
@@ -18,7 +18,7 @@ def selection_sort(arr):
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
     return arr
 
-# 3. insertion sort :             *TC = O(N^^2)
+# 3. insertion sort :  이건 동작방식 한번 그려봐야 겠다.           *TC = O(N^^2)
 def insertion_sort(arr):
     n  = len(arr)
     for i in range(1, n):
@@ -29,3 +29,32 @@ def insertion_sort(arr):
             j -= 1
         arr[j+1] = key
     return arr
+
+# 4. merge sort : 구현의 동작에 재귀적 성질이 있음을 잘 이해하며 코드를 읽어보기   *TC = O(n log n)
+# 정렬 알고리즘은 gif를 보면서 직관적인 이해를 하고 써보면서 한번 더 이해하는 것이 가장 좋다고 생각한다.
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+        merge_sort(left_half)
+        merge_sort(right_half)
+        i = j = k = 0 # index initalize
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+    return arr
+
