67bc1613b9dab7657cda3ea292d21631a82db024
MarkSon-42 <mark42son@gmail.com>
2023-03-30 12:50:43 +0900

    problem solving

--- venv/kookmin_Algolab/_03_공장.py
+++ venv/kookmin_Algolab/_03_공장.py
@@ -1,34 +1,12 @@
 t = int(input())
 
 for i in range(t):
-    n  = int(input())
+    n  = int(input()):
     A = []
     B = []
-    factory = ''
-    aloc = 0
+    msg = ''
     for j in range(n):
-        msg = str(input())
-        if msg[:5] == 'ORDER':
+        order = str(input())
 
-            aloc = int(msg[6])
-            factory = msg[8]
-            if factory == 'A':
-                A.append(aloc)
-            elif factory == 'B':
-                B.append(aloc)
-            else:
-                if sum(A) <= sum(B):
-                    A.append(aloc)
-                else:
-                    B.append(aloc)
-                    # ORDER 처리 끝.
-        else:
-            factory = msg[5]
-            if factory == 'A':
-                del A[0]
-            else:
-                del B[0]
 
-    print(sum(A), sum(B))
 
-# 힌트 : 큐
