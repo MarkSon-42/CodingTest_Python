5177ef3096dc758545644020ec4a56a4119810ff
MarkSon-42 <mark42son@gmail.com>
2023-03-31 13:45:41 +0900

    problem solving python

---
+++ venv/kookmin_Algolab/_04_수고르기.py
@@ -0,0 +1,16 @@
+import sys
+from itertools import combinations
+
+t = int(input())
+
+for i in range(t):
+    n, m = map(int, sys.stdin.readline().split())
+    nums = list(map(int, input().split()))
+    res = combinations(nums, 2)
+
+
+# 두 수의 차이의 모든 경우의 수를 구하는 건가 ? 절대아닌듯
+# 그러면 두 수의 차이중에 m미만인걸 어떻게 바로 걸러내나???
+
+
+
