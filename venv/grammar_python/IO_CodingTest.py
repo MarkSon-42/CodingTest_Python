9310371abf6bc4b4f6a6d3287995660d4d4e6bd3
MarkSon-42 <mark42son@gmail.com>
2023-04-03 13:45:29 +0900

    python

--- venv/grammar_python/IO_CodingTest.py
+++ venv/grammar_python/IO_CodingTest.py
@@ -1,3 +1,19 @@
+
+
 n = int(input()) # read a single integer
 arr = list(map(int, input().split())) # read a list of integers
 s = input().strip() # read a string and strip leading/trailing whitespaces
+
+name = 'Alice'
+age = 25
+print("My name is {0} and I am {1} years old.".format(name, age))
+
+# {index} _ 중괄호에 파라미터 인덱스를 넣는 방식
+
+# Reading input file
+
+with open('input.txt', 'r') as f:
+    n = int(f.readline())
+    arr = list(map(int, f.readline().split()))
+    s = f.readline().strip()
+
