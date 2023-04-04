2da37ecf68c0f0597dc5d11a9887778979c1ab5b
MarkSon-42 <mark42son@gmail.com>
2023-04-04 09:27:59 +0900

    binary search python

---
+++ venv/Algorithms/BinarySearch.py
@@ -0,0 +1,15 @@
+# BS : Binary search is a search algorithm that works by repeatedly dividing the search interval in half.
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+
+    return -1
--- venv/BOJ/Silver/수 찾기.py
+++ venv/BOJ/Silver/수 찾기.py
@@ -1,7 +1,23 @@
-N = int(input())
-N_nums = map(int, input().split())
-M = int(input())
-M_nums = map(int, input().split())
+# N = int(input())
+# N_nums = sorted(list(map(int, input().split())))
+# M = int(input())
+# M_nums = list(map(int, input().split()))
 
-print(N_nums, M_nums)
+# for i in range(len(M_nums)):
+#     if M_nums[i] in N_nums:
+#         print(1)
+#     else:
+#         print(0)
+
+# 해당 코드는 가차없이 시간초과. 자료구조를 쓰지 않았기 때문.
+
+N = int(input())
+N_nums = list(map(int, input().split()))
+M = int(input())
+M_nums = list(map(int, input().split()))
+
+N_nums.sort() # 원본 N_nums의 값을 정렬된 상태로 수정
+
+# N nums의 각 원소별로 이분탐색을 해야한다.
+
 
