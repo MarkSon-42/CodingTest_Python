d148a1aae7d39ecf19547bdbeb984fabdb70b87d
MarkSon-42 <mark42son@gmail.com>
2023-04-07 13:45:20 +0900

    failed

--- venv/kookmin_Algolab/_05_레전드매치.py
+++ venv/kookmin_Algolab/_05_레전드매치.py
@@ -1,7 +1,23 @@
 # import sys
 # input = sys.stdin.readline
+answer = 0
 t = int(input())
 for _ in range(t):
     n, m = map(int, input().split())
     nums = [int(x) for x in input().split()]
-    print(nums)
+    for i in range(len(nums), 2): # 2개씩 봐야 하니까 이렇게>?
+        if nums[i] == max(nums) and nums[i+1] == max(nums): # 최대값이 2개 이상이면,
+            nums[i] += answer
+            nums[i] = -1
+        elif nums[i] == max(nums) and n > 0:
+            nums[i] += answer
+            n -= 1
+        elif nums[i] == max(nums) and i == len(nums)-1:
+            break
+    print(answer)
+
+
+
+
+
+
