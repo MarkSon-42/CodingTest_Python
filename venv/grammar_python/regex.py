5b4259563b704168450ebe62dbfb26f354f242fe
minwoo <mark42son@gmail.com>
2023-04-18 16:31:56 +0900

    regex

--- venv/grammar_python/regex.py
+++ venv/grammar_python/regex.py
@@ -1,2 +1,32 @@
 # 정규식 표현은 문자열 패턴에 매우 유용하다.
 
+
+#
+#     . : matches any single character except a newline.
+#     *
+#     +
+#     ?
+#     ^
+#     $
+#     []
+#     |
+
+import re
+
+pattern = r".at"
+
+match = re.search(pattern, "The cat in the hat.")
+
+if match:
+    print("Pattern found!")
+
+
+pattern = r"[aeiou]"
+
+match = re.search(pattern, "The quick brown fox jumps over the lazxy dog.")
+
+if match:
+    print("Pattern found!")
+
+
+p
