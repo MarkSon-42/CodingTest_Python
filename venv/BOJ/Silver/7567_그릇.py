0f39de326f340110a3c2c2ddcc790436975fb43d
minwoo <mark42son@gmail.com>
2023-04-28 14:47:58 +0900

    add new problems for test

--- venv/BOJ/Silver/7567_그릇.py
+++ venv/BOJ/Silver/7567_그릇.py
@@ -1,7 +1,7 @@
 dish = input()
 answer = 10
 for i in range(len(dish)-1):
-    if dish[i] == dish[i+1]:
+    if dish[i] == dish[i+1]: # 포개지는 경우를 조건문으로 처리하면 그냥 끝나는 문제.
         answer += 5
     else:
         answer += 10
