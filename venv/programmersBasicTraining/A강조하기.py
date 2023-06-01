c524aa69b415a96c2fc01179f9947589ac049682
minwoo <mark42son@gmail.com>
2023-06-01 19:27:41 +0900

    sol5 re

---
+++ venv/programmersBasicTraining/A강조하기.py
@@ -0,0 +1,17 @@
+def solution(myString):
+    answer = ''
+    myString = list(myString)
+    for i in range(len(myString)):
+        if myString[i] == 'a' or myString[i] == 'A':
+            myString[i] = 'A'
+        else:
+            myString[i] = myString[i].lower()
+
+    answer = ''.join(s for s in myString)
+
+    return answer
+
+def solution2(myString):
+    return myString.lower().replace('a', 'A')
+
+# commit message added 'sol5'
