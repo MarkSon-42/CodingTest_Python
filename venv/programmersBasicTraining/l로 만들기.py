a601403fe0d8c9866ffed72596281115ebba2ced
minwoo <mark42son@gmail.com>
2023-06-11 20:10:14 +0900

    sol 24

---
+++ venv/programmersBasicTraining/l로 만들기.py
@@ -0,0 +1,9 @@
+def solution(myString):
+    result = ""
+    for char in myString:
+        if char < "l":
+            result += "l"
+        else:
+            result += char
+    return result
+
