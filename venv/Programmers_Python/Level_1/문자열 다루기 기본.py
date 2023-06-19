81714ce4a3559881ad708ce65edec0e9122fe57f
minwoo <mark42son@gmail.com>
2023-06-19 09:42:58 +0900

    sol 1

---
+++ venv/Programmers_Python/Level_1/문자열 다루기 기본.py
@@ -0,0 +1,21 @@
+def solution(s):
+    if len(s) == 4 or len(s) == 6:
+        if s.isdigit() == True:
+            return True
+        else:
+            return False
+    else:
+        return False
+
+
+
+def alpha_string46(s):
+    #함수를 완성하세요
+
+    return s.isdigit() and len(s) in [4,6]
+
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+print( alpha_string46("a234") )
+print( alpha_string46("1234") )
+
