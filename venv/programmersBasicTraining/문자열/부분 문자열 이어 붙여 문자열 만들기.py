2600bb6a77db7c4383747da335bdb1ebeb941f54
minwoo <mark42son@gmail.com>
2023-06-20 15:15:54 +0900

    0620 sol 1

---
+++ venv/programmersBasicTraining/문자열/부분 문자열 이어 붙여 문자열 만들기.py
@@ -0,0 +1,7 @@
+def solution(my_strings, parts):
+    answer = ''
+    for i in range(len(my_strings)):
+        substring = my_strings[i][parts[i][0]:parts[i][1] + 1]
+        answer += substring
+
+    return answer
