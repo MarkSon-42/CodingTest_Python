8e2277b4d8a84b05731bc7d5cb6d7530b423390d
minwoo <mark42son@gmail.com>
2023-07-04 08:07:23 +0900

    edit : comments

--- venv/programmersBasicTraining/수열과 구간 쿼리 4.py
+++ venv/programmersBasicTraining/수열과 구간 쿼리 4.py
@@ -1,7 +1,7 @@
 def solution(arr, queries):
-    for query in queries: # 쿼리스 목록의 각 쿼리를 반복
-        s, e, k = query # 각 쿼리에 대해
-        for i in range(s, e + 1):
-            if i % k == 0:
+    for query in queries:  # 쿼리스 목록의 각 쿼리를 반복
+        s, e, k = query  # 각 쿼리에 대해 시작 인덱스 s, 끝 인덱스 e, 및 값 k를 추출.
+        for i in range(s, e + 1):  # 그런 다음 s에서 e범위를 반복하고
+            if i % k == 0:  # 각 인덱스 i가 k의 배수인지 확인
                 arr[i] += 1
     return arr
