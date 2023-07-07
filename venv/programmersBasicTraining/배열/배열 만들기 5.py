6156b10a1a72b3bebeae2bd8a69607a58eae6dcb
minwoo <mark42son@gmail.com>
2023-07-07 09:25:57 +0900

    daily algorithm commit

--- venv/programmersBasicTraining/배열/배열 만들기 5.py
+++ venv/programmersBasicTraining/배열/배열 만들기 5.py
@@ -1,1 +1,8 @@
-n = int(input())
+def solution(intStrs, k, s, l):
+    result = []
+    for string in intStrs:
+        substring = string[s:s+l]
+        converted = int(substring)
+        if converted > k:
+            result.append(converted)
+    return result
