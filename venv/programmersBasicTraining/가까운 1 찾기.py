27d5fbf5a3f150abd94e726ce6eaf073fa7bb662
minwoo <mark42son@gmail.com>
2023-07-11 12:28:31 +0900

    commit : daily solving 1st

--- venv/programmersBasicTraining/가까운 1 찾기.py
+++ venv/programmersBasicTraining/가까운 1 찾기.py
@@ -1,8 +1,9 @@
 def solution(arr, idx):
-    n = len(arr)
-
-    for i in range(idx + 1, n):
+    for i in range(idx, len(arr)):
         if arr[i] == 1:
             return i
-
     return -1
+
+# 문제에 오류가 있다
+
+# idx보다 크면서 -> idx와 크거나 같은
