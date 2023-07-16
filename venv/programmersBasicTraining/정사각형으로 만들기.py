69a5557be1ada0c53cb1ad80621439d991d133f6
minwoo <mark42son@gmail.com>
2023-07-17 08:23:47 +0900

    daily problem solving

---
+++ venv/programmersBasicTraining/정사각형으로 만들기.py
@@ -0,0 +1,13 @@
+def solution(arr):
+    rows = len(arr)
+    cols = len(arr[0])
+
+    if rows > cols:
+        for i in range(rows):
+            while len(arr[i]) < rows:
+                arr[i].append(0)
+    elif rows < cols:
+        while len(arr) < cols:
+            arr.append([0] * cols)
+
+    return arr
