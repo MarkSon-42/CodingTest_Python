ce5dab3295d31e9bede2b644f7973d7d1894f0a3
minwoo <mark42son@gmail.com>
2023-07-24 18:53:46 +0900

    learning pycharn debugger

--- venv/Programmers_Python/Level_1/대충 만든 자판.py
+++ venv/Programmers_Python/Level_1/대충 만든 자판.py
@@ -8,6 +8,11 @@ def solution(keymap, targets):
     answer = []
     keytable = {}
     for keys in keymap:
+        for i, key in enumerate(keys):
+            if key not in keytable:
+                keytable[key] = i + 1
+            else:
+                keytable[key] = min(keytable[key], i + 1)
 
     return answer
 
