d0d6ba692cc401d3ee07370512809b3c57146f15
minwoo <mark42son@gmail.com>
2023-08-19 23:16:30 +0900

    added : additional other way function

--- venv/Programmers_Python/Level_1/체육복.py
+++ venv/Programmers_Python/Level_1/체육복.py
@@ -12,3 +12,19 @@ def solution(n, lost, reserve):
 
 
 solution(5, [2, 4], [1, 3, 5])
+
+
+def solution2(n, lost, reserve):
+    lost = set(lost)
+    num_lost = len(lost)
+    reserved = set(reserve) - set(lost)
+    losted = set(lost) - set(reserve)
+
+    for r in sorted(reserved):
+        if r - 1 in losted:
+            losted = losted - {r - 1}
+
+        elif r + 1 in losted:
+            losted = losted - {r + 1}
+
+    return n - len(losted)
