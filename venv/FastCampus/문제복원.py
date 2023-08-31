1998eb24223477ac0df5a86a570025b8c404b911
minwoo <mark42son@gmail.com>
2023-08-31 20:23:51 +0900

    fc coding test

---
+++ venv/FastCampus/문제복원.py
@@ -0,0 +1,12 @@
+# 1 스포츠클라이밍 2차원 배열
+
+# 파라미터에 형변환?
+def solution1(List[str(wall]) -> List[int(wall)]):
+    answer = 0
+    return answer
+
+# 2 강의실 회의대여 시간 , 최대 미팅 가능 횟수
+def solution2():
+
+
+# 3. 스티커 떼기. 앞뒤로 뜯김. 원형큐?
