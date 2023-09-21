495aab98ef6cb83487f2ec2c5671d244031a6116
minwoo <mark42son@gmail.com>
2023-09-21 21:18:26 +0900

    0921 loop pattern codetree

--- venv/CodeTree/Novice Low/별찍기/별 그리기 _ 마름모모양.py
+++ venv/CodeTree/Novice Low/별찍기/별 그리기 _ 마름모모양.py
@@ -1,13 +1,15 @@
+# 요건 노트에 적어두라.. 반복문 개념 + 대칭
+
 n = int(input())
-for i in range(n):
-    for j in range(n - i - 1):
-        print(' ', end='')
-    for j in range(2 * i + 1):
-        print('*', end='')
-    print()
-for i in range(n - 1):
-    for j in range(i + 1):
-        print(' ', end='')
-    for j in range(n - 2 * i):
-        print('*', end='')
-    print()
+
+# 위쪽 반쪽 다이아몬드 그리기
+for i in range(1, n + 1):
+    spaces = " " * (n - i)
+    stars = "*" * (2 * i - 1)
+    print(spaces + stars)
+
+# 아래쪽 반쪽 다이아몬드 그리기
+for i in range(n - 1, 0, -1):
+    spaces = " " * (n - i)
+    stars = "*" * (2 * i - 1)
+    print(spaces + stars)
