fc0b7207e1b1e73ac3a1e98ea392ff5ca74faa70
minwoo <mark42son@gmail.com>
2023-09-21 20:38:35 +0900

    edit string reverse printing

--- venv/CodeTree/Novice Low/문자열/문자열 범위 출력하기 2.py
+++ venv/CodeTree/Novice Low/문자열/문자열 범위 출력하기 2.py
@@ -1,7 +1,8 @@
 word = input()
 n = int(input())
-
-word = word[len(word) - n:]
-
-for i in range(len(word) - 1 , -1, -1):
-    print(word[i], end='')
+if n > len(word):
+    print(word[::-1])
+else:
+    word = word[len(word) - n:]
+    for i in range(len(word) - 1 , -1, -1):
+        print(word[i], end='')
