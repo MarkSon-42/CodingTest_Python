856a172cf49c2cf686bceaaeda221d8ff8aa8d32
minwoo <mark42son@gmail.com>
2023-09-22 15:39:38 +0900

    0922 codetree 2d arrays

--- venv/CodeTree/Novice Low/2차원 배열/두 배열의 곱.py
+++ venv/CodeTree/Novice Low/2차원 배열/두 배열의 곱.py
@@ -1,10 +1,9 @@
+arr1 = [list(map(int,input().split())) for _ in range(3)]
+input()  # 이거 개오반데.. 그래도 알아두자  입력을 뭐 이따위로..
+arr2 = [list(map(int,input().split())) for _ in range(3)]
 
 
-# 두 배열의 같은 위치에 있는 숫자의 곱을 계산하여 결과 배열에 저장합니다.
 for i in range(3):
     for j in range(3):
-        result[i][j] = array1[i][j] * array2[i][j]
-
-# 결과 배열을 출력합니다.
-for row in result:
-    print(row)
+        print(arr1[i][j] * arr2[i][j], end = " ")
+    print()
