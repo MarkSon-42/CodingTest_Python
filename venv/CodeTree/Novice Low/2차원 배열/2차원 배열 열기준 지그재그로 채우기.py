43bd18ec908bf8aaf4a76e392cb885a177d3c4a5
minwoo <mark42son@gmail.com>
2023-09-24 16:07:01 +0900

    codetree 2d array 3rd

---
+++ venv/CodeTree/Novice Low/2차원 배열/2차원 배열 열기준 지그재그로 채우기.py
@@ -0,0 +1,23 @@
+n, m = tuple(map(int, input().split()))
+answer = [
+    [0 for i in range(m)]
+    for _ in range(n)
+]
+
+count = 0
+
+for col in range(m):
+    if col % 2 == 0:
+        for row in range(n):
+            answer[row][col] = count
+            count += 1
+    else:
+        for row in range(n - 1, -1, -1):
+            answer[row][col] = count
+            count += 1
+
+
+for row in range(n):
+    for col in range(m):
+        print(answer[row][col], end=' ')
+    print()
