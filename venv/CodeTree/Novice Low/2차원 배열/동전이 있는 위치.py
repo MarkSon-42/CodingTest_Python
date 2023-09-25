97b3b63bab36c84a0e85419b5bc8f73fe7c8f824
minwoo <mark42son@gmail.com>
2023-09-25 21:52:12 +0900

    codetree 5th 2d arr

---
+++ venv/CodeTree/Novice Low/2차원 배열/동전이 있는 위치.py
@@ -0,0 +1,14 @@
+n, m = tuple(map(int, input().split()))
+
+coin_map = [
+    [0 for _ in range(n + 1)]
+    for _ in range(n + 1)
+]
+for _ in range(m):
+    r, c = map(int, input().split())
+    coin_map[r][c] = 1
+
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        print(coin_map[i][j], end=' ')
+    print()
