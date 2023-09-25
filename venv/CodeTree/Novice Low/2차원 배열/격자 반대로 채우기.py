492cb8292bbde02dec9741b1c83d064fe428330f
minwoo <mark42son@gmail.com>
2023-09-25 16:53:14 +0900

    0925 2d array reverse printing number

---
+++ venv/CodeTree/Novice Low/2차원 배열/격자 반대로 채우기.py
@@ -0,0 +1,35 @@
+n = int(input())
+cnt = 1
+answer = [[0 for _ in range(n)] for _ in range(n)]
+
+if n % 2 != 0:
+    for i in range(n - 1, -1, -1):
+        for j in range(n - 1, -1, -1):
+            if i % 2 == 0:
+                answer[j][i] = cnt
+                cnt += 1
+        for j in range(n):
+            if i % 2 != 0:
+                answer[j][i] = cnt
+                cnt += 1
+
+    for i in range(n):
+        for j in range(n):
+            print(answer[i][j], end=' ')
+        print()
+
+else:
+    for k in range(n - 1, -1, -1):
+        for l in range(n - 1, -1, -1):
+            if k % 2 != 0:
+                answer[l][k] = cnt
+                cnt += 1
+        for l in range(n):
+            if k % 2 == 0:
+                answer[l][k] = cnt
+                cnt += 1
+    for i in range(n):
+        for j in range(n):
+            print(answer[i][j], end=' ')
+        print()
+
