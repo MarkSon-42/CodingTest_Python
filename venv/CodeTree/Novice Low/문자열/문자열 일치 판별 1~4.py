560486664dfb99dd1aab8c9cb2ed22e712af93b0
minwoo <mark42son@gmail.com>
2023-10-09 19:30:46 +0900

    1009 codetree string - 2

--- venv/CodeTree/Novice Low/문자열/문자열 일치 판별 1~4.py
+++ venv/CodeTree/Novice Low/문자열/문자열 일치 판별 1~4.py
@@ -14,3 +14,19 @@ print(cnt)
 
 #    2
 
+# 최대 10회 반복합니다.
+for _ in range(10):
+    # 문자열을 입력받습니다.
+    string = input()
+
+    # str이 END일 경우 종료합니다.
+    if string == "END":
+        break
+
+    # 문자열의 길이를 구합니다.
+    leng = len(string)
+
+    # 문자열을 역으로 출력합니다.
+    for i in range(leng - 1, -1, -1):
+        print(string[i], end="")
+    print()
