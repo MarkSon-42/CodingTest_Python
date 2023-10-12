04b0e4f281a79da1f3787f18b68265a8f27b9c11
minwoo <mark42son@gmail.com>
2023-10-12 15:46:35 +0900

    codetree pq review 1

--- venv/CodeTree/Intermediate Mid/Priority Queue/큰 숫자만 계속 고르기.py
+++ venv/CodeTree/Intermediate Mid/Priority Queue/큰 숫자만 계속 고르기.py
@@ -1,20 +1,16 @@
 import heapq
-
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 pq = []
 
-# priority queue에 숫자 넣어주기.
-# 최대값을 구해야 하므로 미리 - 붙여서 넣어준다.
-
 for elem in arr:
     heapq.heappush(pq, -elem)
 
-    # m번에 걸쳐 최댓값을 찾아 1씩 빼주는 것을 반복
+print(pq)
 
 for _ in range(m):
     max_val = -heapq.heappop(pq)
 
     heapq.heappush(pq, -(max_val - 1))
 
-print(-heapq.heappop(pq))
+print(pq)
