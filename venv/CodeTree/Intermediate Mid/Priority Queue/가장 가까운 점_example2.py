2caad36e2ab53f239bb78ab2edceec76b65f4e92
minwoo <mark42son@gmail.com>
2023-10-13 16:47:48 +0900

    codetree1013

--- venv/CodeTree/Intermediate Mid/Priority Queue/가장 가까운 점_example2.py
+++ venv/CodeTree/Intermediate Mid/Priority Queue/가장 가까운 점_example2.py
@@ -4,3 +4,15 @@ points = [
     (1, 7), (3, 2), (3, 1), (6, 2)
 ]
 pq = []
+
+for point in points:
+    x, y = point
+    heapq.heappush(pq, (-(x * y), x, y)) # priority queue에 넣어줍니다.
+                          # 단, x * y값이 큰 것이 먼저 나오기를 원하므로
+                          # -를 붙여서 넣어줍니다.
+
+    best_point = pq[0]    # x * y 순으로 가장 우선순위가 높은 경우를 찾아줍니다.
+    _, best_x, best_y = best_point # 우리가 원하는 x, y 값만 tuple에서 가져와줍니다.
+
+    print(best_x, best_y)
+
