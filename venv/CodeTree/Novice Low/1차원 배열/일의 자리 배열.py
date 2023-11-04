1ac272c90a8b833dd8cbc5c34b51ae7b0f9e25c2
minwoo <mark42son@gmail.com>
2023-11-04 13:08:26 +0900

    1104 codetree 1st

--- venv/CodeTree/Novice Low/1차원 배열/일의 자리 배열.py
+++ venv/CodeTree/Novice Low/1차원 배열/일의 자리 배열.py
@@ -1,3 +1,13 @@
-# fibo
-a, b = map(int, input().split())
+arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+x, y = map(int, (input().split()))
+arr[0] = x
+arr[1] = y
+
+for i in range(2, len(arr)):
+    arr[i] = arr[i - 2] + arr[i - 1]
+    while arr[i] > 9:
+        arr[i] %= 10
+
+for i in range(len(arr)):
+    print(arr[i], end=' ')
