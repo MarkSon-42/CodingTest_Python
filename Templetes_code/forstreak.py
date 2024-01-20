50b49ce86f8f89bea0c0b4f8763b6426935a76fa
Minwoo Son <mark42son@gmail.com>
2024-01-20 11:55:32 +0900

    Update forstreak.py

--- Templetes_code/forstreak.py
+++ Templetes_code/forstreak.py
@@ -2,3 +2,11 @@ import sys
 
 for _ in range(10):
   print('commit streak is godd but, coding with flow is best of best.')
+
+this_is_tuple = ((a, b), (b, c), (c, d))
+
+for x, y in enumerate(this_is_tuple):
+  print(x, y)
+
+# is valid sentence..?
+  
